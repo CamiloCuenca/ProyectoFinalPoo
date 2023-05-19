3adf1b5fc20c2a98bc43f1aaa36904319fb05da4
twolifelaw <sneid123@hotmail.com>
2023-05-19 11:45:44 -0500

    se elimino las clases shit

--- proyectoFinalTuCarro/src/Controller/Main.java
+++ proyectoFinalTuCarro/src/Controller/Main.java
@@ -1,18 +1,6 @@
 package Controller;
 
-import Model.Moto;
-import Model.Vehiculo;
-import Model.Vehiculo.EsNuevo;
-import Model.Vehiculo.TieneABS;
-import Model.Vehiculo.TieneAireAcondicionado;
-import Model.Vehiculo.TieneAssiPermanencia;
-import Model.Vehiculo.TieneCapReversa;
-import Model.Vehiculo.TieneSensoresColision;
-import Model.Vehiculo.TieneSensoresCrucero;
-import Model.Vehiculo.TieneVelCrucero;
-import Model.Vehiculo.TipoCombustible;
-import Model.Vehiculo.TipoTrasmision;
-import View.Login;
+
 
 public class Main {
 
