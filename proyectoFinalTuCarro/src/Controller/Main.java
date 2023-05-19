aab1f8c7c6b7b7dbddc570548417d88b93095159
Juan Camilo Cuenca Sepulveda <camilocuenca1810@gmail.com>
2023-05-19 17:14:36 -0500

    se creo las clase persona y sus hijas

--- proyectoFinalTuCarro/src/Controller/Main.java
+++ proyectoFinalTuCarro/src/Controller/Main.java
@@ -1,12 +1,24 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import Model.Cliente;
+import Model.Empleados;
+import Model.Persona;
 import View.Login;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Login log = new Login();
-		log.setVisible(true);
+		
+
+		
+	
+		
+		
+		
+		//Login log = new Login();
+		//log.setVisible(true);
 	}
 	
 	
---
+++ proyectoFinalTuCarro/src/Model/Cliente.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Cliente extends Persona {
+
+	public Cliente(String nombre, String apellido, String cedula, String correo, String contraseña) {
+		super(nombre, apellido, cedula, correo, contraseña);
+	}
+
+}
