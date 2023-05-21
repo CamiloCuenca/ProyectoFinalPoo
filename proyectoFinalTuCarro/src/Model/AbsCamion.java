4b7aa18f3c8f914e3925d04a14824e4701488a6b
twolifelaw <sneid123@hotmail.com>
2023-05-21 13:19:48 -0500

    se creo clase abstracta de camion

--- proyectoFinalTuCarro/src/Model/AbsCamion.java
+++ proyectoFinalTuCarro/src/Model/AbsCamion.java
@@ -1,6 +1,20 @@
 package Model;
 
-public class AbsCamion {
+public class AbsCamion extends Vehiculo{
+
+    public AbsCamion(String marca, String modelo, String numPlaca, int cambios, int velocidadMaxima, int cilindraje,
+            String[] fotos, TipoCombustible tipoCombustible, TipoTrasmision tipoTrasmision, EsNuevo esNuevo,
+            TieneABS tieneABS, Disponibilidad disponibilidad) {
+        super(marca, modelo, numPlaca, cambios, velocidadMaxima, cilindraje, fotos, tipoCombustible, tipoTrasmision, esNuevo,
+                tieneABS, disponibilidad);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
     
     
 }
