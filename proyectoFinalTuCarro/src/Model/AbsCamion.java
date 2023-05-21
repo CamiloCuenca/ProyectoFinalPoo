85292a0ca7214a83c11b73f82dee6e1b8ff2ea11
twolifelaw <sneid123@hotmail.com>
2023-05-21 13:46:58 -0500

    se creo la clase vans

--- proyectoFinalTuCarro/src/Model/AbsCamion.java
+++ proyectoFinalTuCarro/src/Model/AbsCamion.java
@@ -1,18 +1,19 @@
 package Model;
 
 public class AbsCamion extends Vehiculo{
+    private int numEjes;
 
     public AbsCamion(String marca, String modelo, String numPlaca, int cambios, int velocidadMaxima, int cilindraje,
             String[] fotos, TipoCombustible tipoCombustible, TipoTrasmision tipoTrasmision, EsNuevo esNuevo,
             TieneABS tieneABS, Disponibilidad disponibilidad) {
         super(marca, modelo, numPlaca, cambios, velocidadMaxima, cilindraje, fotos, tipoCombustible, tipoTrasmision, esNuevo,
                 tieneABS, disponibilidad);
-        //TODO Auto-generated constructor stub
+
     }
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
+        
         throw new UnsupportedOperationException("Unimplemented method 'toString'");
     }
    
