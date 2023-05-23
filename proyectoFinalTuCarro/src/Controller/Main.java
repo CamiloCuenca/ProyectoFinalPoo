2f027566cf90ad99b9d1a819947a7466dd3359b3
twolifelaw <sneid123@hotmail.com>
2023-05-23 00:07:53 -0500

    se agrego cometarios y orden a cada clase

--- proyectoFinalTuCarro/src/Controller/Main.java
+++ proyectoFinalTuCarro/src/Controller/Main.java
@@ -1,23 +1,26 @@
 package Controller;
 
-
 import java.util.ArrayList;
 
 import Model.Cliente;
 import Model.Empleados;
+import Model.Moto;
 import Model.Persona;
+import Model.Vehiculo.Disponibilidad;
+import Model.Vehiculo.EsNuevo;
+import Model.Vehiculo.TipoCombustible;
+import Model.Vehiculo.TipoTrasmision;
 import View.Login;
 import View.registrar;
-
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		registrar r = new registrar();
-		r.setVisible(true);
-		
-		
+		Moto moto = new Moto("YAMAHA", "2010", "112A", 6, 150, 1500, args, TipoCombustible.DISEL,
+				TipoTrasmision.AUTOMATICO, EsNuevo.SI, Disponibilidad.DISPONIBLE);
+
+		System.out.println(moto.toString());
+
 	}
 
 }
--- proyectoFinalTuCarro/src/Model/Camion.java
+++ proyectoFinalTuCarro/src/Model/Camion.java
@@ -1,6 +1,7 @@
 package Model;
 
 public class Camion extends AbsVehiculoCarga {
+    // Atributos
     private int capacidadCarga;
 
     public enum TieneFrenoAire {
@@ -10,15 +11,17 @@ public class Camion extends AbsVehiculoCarga {
     TieneFrenoAire tieneFrenoAire;
 
     public enum TipoCamion {
-        CAMIONRIGIDO,CAMIONARTICULADO
+        CAMIONRIGIDO, CAMIONARTICULADO
     };
 
     TipoCamion tipoCamion;
 
+    // Constructor
     public Camion(String marca, String modelo, String numPlaca, int cambios, int velocidadMaxima, int cilindraje,
-            int numEjes, int numBolasAire,int capacidadCarga, String[] fotos, TipoCombustible tipoCombustible,
+            int numEjes, int numBolasAire, int capacidadCarga, String[] fotos, TipoCombustible tipoCombustible,
             TipoTrasmision tipoTrasmision, EsNuevo esNuevo, Disponibilidad disponibilidad,
-            TieneAireAcondicionado tieneAireAcondicionado, TieneABS tieneABS,TieneFrenoAire tieneFrenoAire, TipoCamion tipoCamion) {
+            TieneAireAcondicionado tieneAireAcondicionado, TieneABS tieneABS, TieneFrenoAire tieneFrenoAire,
+            TipoCamion tipoCamion) {
         super(marca, modelo, numPlaca, cambios, velocidadMaxima, cilindraje, numEjes, numBolasAire, fotos,
                 tipoCombustible,
                 tipoTrasmision, esNuevo, disponibilidad, tieneAireAcondicionado, tieneABS);
@@ -27,19 +30,45 @@ public class Camion extends AbsVehiculoCarga {
         this.tipoCamion = tipoCamion;
     }
 
+    // Metodo imprimir
     @Override
     public String toString() {
         String v = " Camion " + ", \nMarca: " + getMarca() + ", \nModelo: " + getModelo() + ", \nNumero Placa: "
                 + getNumPlaca() + ", \nCambios: " + getCambios() + ", \nVelodidadMaxima: " + getVelocidadMaxima()
-                + ", \nCilindraje: " + getCilindraje() 
+                + ", \nCilindraje: " + getCilindraje()
                 + ", \nNumero de bolsas de aire: " + getNumBolasAire()
-                + ", \nCapacidad Carga : " + capacidadCarga + ", \nEjes: " +getNumEjes() 
+                + ", \nCapacidad Carga : " + capacidadCarga + ", \nEjes: " + getNumEjes()
                 + ", \nTipo Combustible: " + getTipoCombustible()
                 + ", \nTipo Trasmisión: " + getTipoTrasmision() + "; \n Es nuevo: " + getEsNuevo()
-                + ", \nDisponibilidad: " + getDisponibilidad() 
-                + ", \nAire acondicionado: " + tieneAireAcondicionado 
+                + ", \nDisponibilidad: " + getDisponibilidad()
+                + ", \nAire acondicionado: " + tieneAireAcondicionado
                 + ", \nABS: " + tieneABS;
         return v;
+    }
+
+    // GETTERS Y SETTRES
+    public int getCapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(int capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    public TieneFrenoAire getTieneFrenoAire() {
+        return tieneFrenoAire;
+    }
+
+    public void setTieneFrenoAire(TieneFrenoAire tieneFrenoAire) {
+        this.tieneFrenoAire = tieneFrenoAire;
+    }
+
+    public TipoCamion getTipoCamion() {
+        return tipoCamion;
+    }
+
+    public void setTipoCamion(TipoCamion tipoCamion) {
+        this.tipoCamion = tipoCamion;
     }
 
 }
